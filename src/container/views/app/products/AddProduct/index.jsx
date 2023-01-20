7db5b4d353a001f6d498012d53921a6fa4a59c3f
Maryam <89831604+MaryamEsmaily@users.noreply.github.com>
2023-01-20 20:02:24 +0330

    update AddProductPage

---
+++ src/container/views/app/products/AddProduct/index.jsx
@@ -0,0 +1,119 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Grid,
+  GridItem,
+  Input,
+  Stack,
+  Textarea,
+} from "@chakra-ui/react";
+import { useFormik } from "formik";
+import React from "react";
+//
+const initialValues = {
+  productName: "",
+  sizes: [],
+  description: "",
+  code: "",
+  colors: [],
+  images: [],
+};
+//
+function AddProduct() {
+  //
+  const handleSubmit = () => {};
+  //
+  const formik = useFormik({
+    onSubmit: handleSubmit,
+    initialValues: initialValues,
+  });
+  //
+  return (
+    <Box
+      mt={8}
+      borderRadius={24}
+      bg="layout-over"
+      p={10}
+      as="form"
+      onSubmit={formik.handleSubmit}
+    >
+      <Grid templateColumns="repeat(2,minmax(0,1fr))" gap={10}>
+        <GridItem colSpan={1}>
+          <FormControl isRequired>
+            <FormLabel fontWeight="bold">نـام محصول </FormLabel>
+            <Input
+              size="lg"
+              variant="filled"
+              _placeholder={{ color: "text-primary" }}
+              placeholder="نـام محصول"
+              {...formik.getFieldProps("productName")}
+            />
+          </FormControl>
+          <FormControl mt={10} isRequired>
+            <FormLabel fontWeight="bold">سایز بندی </FormLabel>
+            <Input
+              size="lg"
+              variant="filled"
+              {...formik.getFieldProps("sizes")}
+            />
+          </FormControl>
+          <FormControl mt={10}>
+            <FormLabel fontWeight="bold">توضیحـات</FormLabel>
+            <Textarea
+              h="170px"
+              variant="filled"
+              _placeholder={{ color: "text-primary" }}
+              placeholder="توضیحـات بیشتر ..."
+              {...formik.getFieldProps("description")}
+            />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <FormControl isRequired>
+            <FormLabel fontWeight="bold">کـد طرح </FormLabel>
+            <Input
+              size="lg"
+              variant="filled"
+              _placeholder={{ color: "text-primary" }}
+              placeholder="کـد طرح "
+              {...formik.getFieldProps("code")}
+            />
+          </FormControl>
+          <FormControl mt={10} isRequired>
+            <FormLabel fontWeight="bold">رنگ بندی </FormLabel>
+            <Input
+              size="lg"
+              variant="filled"
+              {...formik.getFieldProps("colors")}
+            />
+          </FormControl>
+          <FormControl mt={10} isRequired>
+            <FormLabel fontWeight="bold">افزودن عکس </FormLabel>
+            <Textarea
+              h="170px"
+              variant="filled"
+              _placeholder={{ color: "text-primary" }}
+              placeholder="پیوست فـایـل"
+              {...formik.getFieldProps("images")}
+            />
+            <Input
+              size="lg"
+              variant="filled"
+              {...formik.getFieldProps("images")}
+            />
+          </FormControl>
+        </GridItem>
+      </Grid>
+      <Stack direction="row" spacing={2} mt={6} w="full" justify="end">
+        <Button w={140} variant="outline">
+          انصراف
+        </Button>
+        <Button w={140}>ثبت اطلاعات</Button>
+      </Stack>
+    </Box>
+  );
+}
+
+export default AddProduct;
--- src/views/app/products/AddProduct.jsx
+++ src/views/app/products/AddProduct.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import {
+  Box,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import BasketIcon from "components/icon/BasketIcon";
+import AddProduct from "container/views/app/products/AddProduct";
 
 function AddProductPage() {
-  return <div>AddProductPage</div>;
+  return (
+    <Box>
+      <Stack direction="row" alignItems="center" spacing={2}>
+        <BasketIcon boxSize={5} />
+        <Text fontWeight="bold" fontSize={20}>
+          افـزودن محصول
+        </Text>
+      </Stack>
+      <Breadcrumb
+        mt={3}
+        color="text-primary"
+        fontSize={18}
+        separator={<Box width={1} height={1} bg="text-primary" />}
+      >
+        <BreadcrumbItem>
+          <Box width={1} height={1} ml={2} bg="text-primary" />
+          <BreadcrumbLink href="#">داشبورد</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="#">افزودن محصول</BreadcrumbLink>
+        </BreadcrumbItem>
+      </Breadcrumb>
+      <AddProduct />
+    </Box>
+  );
 }
 
 export default AddProductPage;
