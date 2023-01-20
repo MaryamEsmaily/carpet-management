ef22beadb52054a090f5d9847357c55fd77e27ea
Maryam <89831604+MaryamEsmaily@users.noreply.github.com>
2023-01-20 13:13:55 +0330

    update style config

--- src/theme/styles.js
+++ src/theme/styles.js
@@ -1,7 +1,12 @@
 const styles = {
   global: {
     "html, body": {
+      bg: "black",
+    },
+    "#root": {
       bg: "layout-deep",
+      borderRadius: "38px",
+      overflow: "hidden",
     },
     ".js-focus-visible :focus:not([data-focus-visible-added])": {
       outline: "none",
