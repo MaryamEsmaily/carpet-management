6e838370f3a402964a548926a253efd36a1ad037
Maryam <89831604+MaryamEsmaily@users.noreply.github.com>
2023-01-20 18:35:55 +0330

    add image and icon

---
+++ src/components/icon/DeleteIcon/index.jsx
@@ -0,0 +1,46 @@
+import { Icon } from "@chakra-ui/react";
+import React from "react";
+
+function DeleteIcon(props) {
+  return (
+    <Icon viewBox="0 0 20 20" {...props}>
+      <path
+        d="M17.5 4.98368C14.725 4.70868 11.9333 4.56702 9.15 4.56702C7.5 4.56702 5.85 4.65035 4.2 4.81702L2.5 4.98368"
+        stroke="currentColor"
+        strokeWidth="1.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.08331 4.14163L7.26665 3.04996C7.39998 2.25829 7.49998 1.66663 8.90831 1.66663H11.0916C12.5 1.66663 12.6083 2.29163 12.7333 3.05829L12.9166 4.14163"
+        stroke="currentColor"
+        strokeWidth="1.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15.7087 7.61633L15.167 16.008C15.0753 17.3163 15.0003 18.333 12.6753 18.333H7.32533C5.00033 18.333 4.92533 17.3163 4.83366 16.008L4.29199 7.61633"
+        stroke="currentColor"
+        strokeWidth="1.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.6084 13.75H11.3834"
+        stroke="currentColor"
+        strokeWidth="1.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.91669 10.4166H12.0834"
+        stroke="currentColor"
+        strokeWidth="1.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Icon>
+  );
+}
+
+export default DeleteIcon;
