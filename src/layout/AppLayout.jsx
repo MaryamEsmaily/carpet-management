ab44d323ba3cf1bc8f09c71b7f3f2fe887ebae7b
Maryam <89831604+MaryamEsmaily@users.noreply.github.com>
2023-01-24 22:50:32 +0330

    remove change lang icon from appLayout

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,3 +1,4 @@
+import { memo, useEffect } from "react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import {
   Avatar,
@@ -8,14 +9,11 @@ import {
   Stack,
   Text,
   useColorMode,
-  useColorModeValue,
 } from "@chakra-ui/react";
 import CallIcon from "components/icon/CallIcon";
 import LogoIcon from "components/icon/LogoIcon";
 import SearchIcon from "components/icon/SearchIcon";
-import TranslateIcon from "components/icon/TranslateIcon";
 import { menuItems } from "constant/menuItems";
-import { memo, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
 
@@ -23,16 +21,17 @@ const AppLayout = (props) => {
   // theme stuff
   const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === "dark";
-  const iconColor = useColorModeValue("#000", "#fff");
 
   // language stuff
-  const { i18n, t } = useTranslation();
-
-  useEffect(() => {
-    document.dir = i18n.dir();
-  }, [i18n, i18n.language]);
-
-  const currentLang = i18n.language;
+  const { t } = useTranslation();
+  //
+  // const iconColor = useColorModeValue("#000", "#fff");
+  //
+  // useEffect(() => {
+  //   document.dir = i18n.dir();
+  // }, [i18n, i18n.language]);
+  //
+  // const currentLang = i18n.language;
   //
   return (
     <>
@@ -88,21 +87,6 @@ const AppLayout = (props) => {
                 <SunIcon fontSize="20px" />
               )
             }
-          />
-          <IconButton
-            onClick={() => {
-              window.location.reload();
-              if (currentLang === "fa") {
-                localStorage.setItem("locale", "en");
-                i18n.changeLanguage("en");
-              } else {
-                localStorage.setItem("locale", "fa");
-                i18n.changeLanguage("fa");
-              }
-            }}
-            size="sm"
-            variant="unstyled"
-            icon={<TranslateIcon color={iconColor} boxSize={5} />}
           />
           <SearchIcon onClick={toggleColorMode} fill="none" boxSize={5} />
           <CallIcon fill="none" boxSize={5} />
