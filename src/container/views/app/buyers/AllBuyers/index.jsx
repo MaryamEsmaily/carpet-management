0612efb7609ce3e46bb8a2e4e4bf4edbf351bec1
Maryam <89831604+MaryamEsmaily@users.noreply.github.com>
2023-02-12 01:01:47 +0330

    fix style issue

--- src/container/views/app/buyers/AllBuyers/index.jsx
+++ src/container/views/app/buyers/AllBuyers/index.jsx
@@ -8,6 +8,7 @@ import {
   PopoverContent,
   PopoverTrigger,
   Radio,
+  SimpleGrid,
   Stack,
   Text,
 } from "@chakra-ui/react";
@@ -103,111 +104,98 @@ function AllBuyers({ filterSearch, status }) {
     <Box fontSize="sm">
       <Grid mt={5} templateColumns="repeat(1,minmax(0,1fr))" gap={7}>
         {data?.map((buyer, index) => (
-          <GridItem key={buyer.id} colSpan={{ base: 2, lg: 1 }}>
-            <Stack
-              justifyContent="space-between"
+          <GridItem key={buyer.id} colSpan={1}>
+            <SimpleGrid
               borderRadius={14}
               bg={index % 2 === 0 ? "layout" : "layout-over"}
               p={6}
+              columns={{ base: 1, lg: 2 }}
+              spacing={6}
+              position="relative"
             >
-              <Stack
-                direction="row"
-                align="start"
-                justify="space-between"
-                spacing={4}
-              >
-                <Stack spacing={5}>
-                  <Stack direction="row" align="center" spacing={2}>
-                    <UserIcon color="text-primary" boxSize={5} />
-                    <Text color="text-primary">نـام و نـام خـانوادگی :</Text>
-                    <Text
-                      _hover={{ textDecoration: "underline" }}
-                      noOfLines={1}
+              <Stack direction="row" align="center" spacing={2}>
+                <UserIcon color="text-primary" boxSize={5} />
+                <Text color="text-primary">نـام و نـام خـانوادگی :</Text>
+                <Text _hover={{ textDecoration: "underline" }} noOfLines={1}>
+                  {buyer.fullName}
+                </Text>
+              </Stack>
+              <Stack direction="row" align="center" spacing={2}>
+                <PhoneIcon color="text-primary" boxSize={5} />
+                <Text color="text-primary">شمـاره همـراه :</Text>
+                <Text>{buyer.mobileNumber}</Text>
+              </Stack>
+              <Stack direction="row" align="center" spacing={2}>
+                <MessagesIcon color="text-primary" boxSize={5} />
+                <Text color="text-primary">پست الکترونیک :</Text>
+                <Text>{buyer.email}</Text>
+              </Stack>
+              <Stack direction="row" align="center" spacing={2}>
+                <LocationIcon color="text-primary" fill="none" boxSize={5} />
+                <Text color="text-primary">آدرس :</Text>
+                <Text>{buyer.address}</Text>
+              </Stack>
+              <Popover placement="left-start">
+                <PopoverTrigger>
+                  <IconButton
+                    position="absolute"
+                    top={3}
+                    right={2}
+                    icon={<MoreIcon boxSize={4} />}
+                    variant="unstyled"
+                  />
+                </PopoverTrigger>
+                <PopoverContent
+                  sx={{ width: "140px", borderRadius: "8px", p: 4 }}
+                >
+                  <PopoverArrow />
+                  <Stack spacing={4}>
+                    <Radio
+                      size="lg"
+                      isChecked={buyer.status === "0"}
+                      onClick={() =>
+                        handleStatus({
+                          id: buyer?.id,
+                          status: buyer?.status === "0" ? "1" : "0",
+                        })
+                      }
                     >
-                      {buyer.fullName}
-                    </Text>
-                  </Stack>
-                  <Stack direction="row" align="center" spacing={2}>
-                    <MessagesIcon color="text-primary" boxSize={5} />
-                    <Text color="text-primary">پست الکترونیک :</Text>
-                    <Text>{buyer.email}</Text>
-                  </Stack>
-                </Stack>
-                <Stack spacing={5}>
-                  <Stack direction="row" align="center" spacing={2}>
-                    <PhoneIcon color="text-primary" boxSize={5} />
-                    <Text color="text-primary">شمـاره همـراه :</Text>
-                    <Text>{buyer.mobileNumber}</Text>
-                  </Stack>
-                  <Stack direction="row" align="center" spacing={2}>
-                    <LocationIcon
-                      color="text-primary"
-                      fill="none"
-                      boxSize={5}
-                    />
-                    <Text color="text-primary">آدرس :</Text>
-                    <Text>{buyer.address}</Text>
-                  </Stack>
-                </Stack>
-                <Popover placement="left-start">
-                  <PopoverTrigger>
-                    <IconButton
-                      icon={<MoreIcon boxSize={4} />}
-                      variant="unstyled"
-                    />
-                  </PopoverTrigger>
-                  <PopoverContent
-                    sx={{ width: "140px", borderRadius: "8px", p: 4 }}
-                  >
-                    <PopoverArrow />
-                    <Stack spacing={4}>
-                      <Radio
-                        size="lg"
-                        isChecked={buyer.status === "0"}
-                        onClick={() =>
-                          handleStatus({
-                            id: buyer?.id,
-                            status: buyer?.status === "0" ? "1" : "0",
-                          })
-                        }
-                      >
-                        <Text fontSize="md" mx={1}>
-                          {/* غیـرفعـال */}
-                          {t("13")}
-                        </Text>
-                      </Radio>
-                      <Link to={`buyer-edit/${buyer.id}`}>
-                        <Stack
-                          cursor="pointer"
-                          direction="row"
-                          align="center"
-                          spacing={3}
-                        >
-                          <EditIcon boxSize={5} />
-                          <Text>
-                            {/* ویرایش */}
-                            {t("20")}
-                          </Text>
-                        </Stack>
-                      </Link>
+                      <Text fontSize="md" mx={1}>
+                        {/* غیـرفعـال */}
+                        {t("13")}
+                      </Text>
+                    </Radio>
+                    <Link to={`buyer-edit/${buyer.id}`}>
                       <Stack
                         cursor="pointer"
                         direction="row"
                         align="center"
                         spacing={3}
-                        onClick={() => handleDelete(buyer.id)}
                       >
-                        <DeleteIcon fill="none" color="red" boxSize={6} />
+                        <EditIcon boxSize={5} />
                         <Text>
-                          {/* حذف */}
-                          {t("21")}
+                          {/* ویرایش */}
+                          {t("20")}
                         </Text>
                       </Stack>
+                    </Link>
+                    <Stack
+                      cursor="pointer"
+                      direction="row"
+                      align="center"
+                      spacing={3}
+                      onClick={() => handleDelete(buyer.id)}
+                    >
+                      <DeleteIcon fill="none" color="red" boxSize={6} />
+                      <Text>
+                        {/* حذف */}
+                        {t("21")}
+                      </Text>
                     </Stack>
-                  </PopoverContent>
-                </Popover>
-              </Stack>
-            </Stack>
+                  </Stack>
+                </PopoverContent>
+              </Popover>
+            </SimpleGrid>
           </GridItem>
         ))}
       </Grid>
--- src/container/views/app/store/AllStores/index.jsx
+++ src/container/views/app/store/AllStores/index.jsx
@@ -8,6 +8,7 @@ import {
   PopoverContent,
   PopoverTrigger,
   Radio,
+  SimpleGrid,
   Stack,
   Text,
 } from "@chakra-ui/react";
@@ -101,116 +102,101 @@ function AllStores({ filterSearch, status }) {
     <Box fontSize="sm">
       <Grid mt={5} templateColumns="repeat(1,minmax(0,1fr))" gap={7}>
         {data?.map((store, index) => (
-          <GridItem key={store.id} colSpan={{ base: 2, lg: 1 }}>
-            <Stack
-              justifyContent="space-between"
+          <GridItem key={store.id} colSpan={1}>
+            <SimpleGrid
               borderRadius={14}
               bg={index % 2 === 0 ? "layout" : "layout-over"}
               p={6}
+              columns={{ base: 1, lg: 3 }}
+              spacing={6}
+              position="relative"
             >
-              <Stack
-                direction="row"
-                align="start"
-                justify="space-between"
-                spacing={4}
-              >
-                <Stack spacing={5}>
-                  <Stack direction="row" align="center" spacing={2}>
-                    <StoreIcon color="text-primary" boxSize={5} />
-                    <Text color="text-primary">نـام :</Text>
-                    <Text
-                      _hover={{ textDecoration: "underline" }}
-                      noOfLines={1}
+              <Stack direction="row" align="center" spacing={2}>
+                <StoreIcon color="text-primary" boxSize={5} />
+                <Text color="text-primary">نـام :</Text>
+                <Text>{store.title}</Text>
+              </Stack>
+              <Stack direction="row" align="center" spacing={2}>
+                <StarIcon color="text-primary" boxSize={5} />
+                <Text color="text-primary">نوع انبـار :</Text>
+                <Text>{store.storeType}</Text>
+              </Stack>
+              <Stack direction="row" align="center" spacing={2}>
+                <UserIcon color="text-primary" fill="none" boxSize={5} />
+                <Text color="text-primary">انبـاردار :</Text>
+                <Text>{store.fullName}</Text>
+              </Stack>
+              <Stack direction="row" align="center" spacing={2}>
+                <PhoneIcon color="text-primary" boxSize={5} />
+                <Text color="text-primary">شمـاره تلفـن :</Text>
+                <Text>{store.mobileNumber}</Text>
+              </Stack>
+              <Stack direction="row" align="center" spacing={2}>
+                <LocationIcon color="text-primary" fill="none" boxSize={5} />
+                <Text color="text-primary">آدرس :</Text>
+                <Text>{store.address}</Text>
+              </Stack>
+              <Popover placement="left-start">
+                <PopoverTrigger>
+                  <IconButton
+                    position="absolute"
+                    top={3}
+                    right={2}
+                    icon={<MoreIcon boxSize={4} />}
+                    variant="unstyled"
+                  />
+                </PopoverTrigger>
+                <PopoverContent
+                  sx={{ width: "140px", borderRadius: "8px", p: 4 }}
+                >
+                  <PopoverArrow />
+                  <Stack spacing={4}>
+                    <Radio
+                      size="lg"
+                      isChecked={store.status === "0"}
+                      onClick={() =>
+                        handleStatus({
+                          id: store?.id,
+                          status: store?.status === "0" ? "1" : "0",
+                        })
+                      }
                     >
-                      {store.title}
-                    </Text>
-                  </Stack>
-                  <Stack direction="row" align="center" spacing={2}>
-                    <PhoneIcon color="text-primary" boxSize={5} />
-                    <Text color="text-primary">شمـاره تلفـن :</Text>
-                    <Text>{store.mobileNumber}</Text>
-                  </Stack>
-                </Stack>
-                <Stack spacing={5}>
-                  <Stack direction="row" align="center" spacing={2}>
-                    <StarIcon color="text-primary" boxSize={5} />
-                    <Text color="text-primary">نوع انبـار :</Text>
-                    <Text>{store.storeType}</Text>
-                  </Stack>
-                  <Stack direction="row" align="center" spacing={2}>
-                    <LocationIcon
-                      color="text-primary"
-                      fill="none"
-                      boxSize={5}
-                    />
-                    <Text color="text-primary">آدرس :</Text>
-                    <Text>{store.address}</Text>
-                  </Stack>
-                </Stack>
-                <Stack direction="row" align="center" spacing={2}>
-                  <UserIcon color="text-primary" fill="none" boxSize={5} />
-                  <Text color="text-primary">انبـاردار :</Text>
-                  <Text>{store.fullName}</Text>
-                </Stack>
-                <Popover placement="left-start">
-                  <PopoverTrigger>
-                    <IconButton
-                      icon={<MoreIcon boxSize={4} />}
-                      variant="unstyled"
-                    />
-                  </PopoverTrigger>
-                  <PopoverContent
-                    sx={{ width: "140px", borderRadius: "8px", p: 4 }}
-                  >
-                    <PopoverArrow />
-                    <Stack spacing={4}>
-                      <Radio
-                        size="lg"
-                        isChecked={store.status === "0"}
-                        onClick={() =>
-                          handleStatus({
-                            id: store?.id,
-                            status: store?.status === "0" ? "1" : "0",
-                          })
-                        }
-                      >
-                        <Text fontSize="md" mx={1}>
-                          {/* غیـرفعـال */}
-                          {t("13")}
-                        </Text>
-                      </Radio>
-                      <Link to={`store-edit/${store.id}`}>
-                        <Stack
-                          cursor="pointer"
-                          direction="row"
-                          align="center"
-                          spacing={3}
-                        >
-                          <EditIcon boxSize={5} />
-                          <Text>
-                            {/* ویرایش */}
-                            {t("20")}
-                          </Text>
-                        </Stack>
-                      </Link>
+                      <Text fontSize="md" mx={1}>
+                        {/* غیـرفعـال */}
+                        {t("13")}
+                      </Text>
+                    </Radio>
+                    <Link to={`store-edit/${store.id}`}>
                       <Stack
                         cursor="pointer"
                         direction="row"
                         align="center"
                         spacing={3}
-                        onClick={() => handleDelete(store.id)}
                       >
-                        <DeleteIcon fill="none" color="red" boxSize={6} />
+                        <EditIcon boxSize={5} />
                         <Text>
-                          {/* حذف */}
-                          {t("21")}
+                          {/* ویرایش */}
+                          {t("20")}
                         </Text>
                       </Stack>
+                    </Link>
+                    <Stack
+                      cursor="pointer"
+                      direction="row"
+                      align="center"
+                      spacing={3}
+                      onClick={() => handleDelete(store.id)}
+                    >
+                      <DeleteIcon fill="none" color="red" boxSize={6} />
+                      <Text>
+                        {/* حذف */}
+                        {t("21")}
+                      </Text>
                     </Stack>
-                  </PopoverContent>
-                </Popover>
-              </Stack>
-            </Stack>
+                  </Stack>
+                </PopoverContent>
+              </Popover>
+            </SimpleGrid>
           </GridItem>
         ))}
       </Grid>
