29ecc5b169f3cff2b26b1e11bfd1680961f6aca8
Maryam <89831604+MaryamEsmaily@users.noreply.github.com>
2023-02-26 18:40:09 +0330

    test for cartable icon

--- src/components/icon/CartableIcon/index.jsx
+++ src/components/icon/CartableIcon/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function CartableIcon(props) {
   return (
-    <Icon viewBox="0 0 24 24" fill="none" {...props}>
+    <Icon viewBox="0 0 24 24" fill="none" userSelect="none" {...props}>
       <path
         d="M22 8.27V4.23C22 2.64 21.36 2 19.77 2H15.73C14.14 2 13.5 2.64 13.5 4.23V8.27C13.5 9.86 14.14 10.5 15.73 10.5H19.77C21.36 10.5 22 9.86 22 8.27Z"
         stroke="currentColor"
