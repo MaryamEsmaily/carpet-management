c33c5780e1dbada97d2878d230774c4b02004bfb
Maryam <89831604+MaryamEsmaily@users.noreply.github.com>
2023-03-10 01:17:24 +0330

    add tooltip to input

--- src/container/views/app/employees/EmployeeProfileDetails/index.jsx
+++ src/container/views/app/employees/EmployeeProfileDetails/index.jsx
@@ -6,8 +6,12 @@ import {
   FormLabel,
   Grid,
   GridItem,
+  IconButton,
+  InputGroup,
+  InputLeftElement,
   Stack,
   Textarea,
+  Tooltip,
 } from "@chakra-ui/react";
 import Uploader from "components/Uploader";
 import Input from "components/custom/Input";
@@ -15,6 +19,7 @@ import AccountIcon from "components/icon/AccountIcon";
 import InputSelectList from "components/custom/InputSelectList";
 import getValidationFieldProps from "utils/getValidationFieldProps";
 import FormHelperMessage from "components/FormHelperMessage";
+import RoundedInfoIcon from "components/icon/RoundedInfoIcon";
 //
 function EmployeeProfileDetails({ formik }) {
   return (
@@ -39,13 +44,56 @@ function EmployeeProfileDetails({ formik }) {
           </FormControl>
           <FormControl mt={10} isRequired>
             <FormLabel fontWeight="bold">نام کاربری </FormLabel>
-            <Input
-              variant="filled"
-              _placeholder={{ color: "text-primary" }}
-              placeholder="نام کاربری "
-              {...formik.getFieldProps("userName")}
-              {...getValidationFieldProps(formik, "userName")}
-            />
+            <InputGroup>
+              <Input
+                variant="filled"
+                _placeholder={{ color: "text-primary" }}
+                placeholder="نام کاربری "
+                {...formik.getFieldProps("userName")}
+                {...getValidationFieldProps(formik, "userName")}
+              />
+              <InputLeftElement
+                sx={{
+                  right: 0,
+                  left: "unset",
+                }}
+              >
+                <Tooltip
+                  rounded={6}
+                  width={200}
+                  p={3}
+                  fontSize="xs"
+                  label={
+                    <Stack>
+                      <li>
+                        رمز عبور باید حداقل ۸ کاراکتر و شامل حروف کوچک و بزرگ،
+                        اعداد و نمادها باشد
+                      </li>
+                      <li>قانون شماره دو برای نام کاربری</li>
+                    </Stack>
+                  }
+                  placement="right-start"
+                  bg="layout-deep"
+                  color="text-secondary"
+                >
+                  <IconButton
+                    icon={<RoundedInfoIcon boxSize={5} color="text-primary" />}
+                    borderRadius="lg"
+                    variant="link"
+                    // onClick={handleClick}
+                  />
+                </Tooltip>
+                {/* <Tooltip label="Phone number" fontSize="md">
+                  <RoundedInfoIcon boxSize={5} color="text-primary" />
+                </Tooltip> */}
+                {/* <IconButton
+                  icon={<RoundedInfoIcon boxSize={5} color="text-primary" />}
+                  borderRadius="lg"
+                  variant="link"
+                  // onClick={handleClick}
+                /> */}
+              </InputLeftElement>
+            </InputGroup>
             <FormHelperMessage formik={formik} name="userName" />
           </FormControl>
           <FormControl mt={10} isRequired>
